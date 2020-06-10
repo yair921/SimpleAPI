369bbc60c8e82c245f8cb428a55effad5480eaf6
Yair <yair_918@hotmail.com>
2020-06-10 16:45:16 -0500

    Fixi unit test

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -40,7 +40,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "test 1";
+            return "test";
         }
     }
 }
